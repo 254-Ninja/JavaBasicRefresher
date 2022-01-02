7d8c5bb37e1fc9f60aef0aa693cc7756b6e12ef5
User <muthomidenis62@gmail.com>
2022-01-02 12:06:57 +0300

    Reversing a string in a java init commit

--- app/src/main/java/com/example/javabasicrefresher/Reversing_a_string_in_Java.java
+++ app/src/main/java/com/example/javabasicrefresher/Reversing_a_string_in_Java.java
@@ -5,5 +5,14 @@ public class Reversing_a_string_in_Java {
     public static void main (String [] args) {
 
         String str = "123";
+        System.out.println(reverse(str));
+    }
+
+    public static String reverse(String in) {
+        if (in == null)
+            throw new IllegalArgumentException("Null is not valid input");
+        StringBuilder out = new StringBuilder();
+
+        char [] chars = in. toCharArray();
     }
 }
