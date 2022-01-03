40a4da1e3e5d7f7e9743c18f0ee823a10365a3a3
User <muthomidenis62@gmail.com>
2022-01-03 12:42:08 +0300

    Find if a number is prime or not

---
+++ app/src/main/java/com/example/javabasicrefresher/findIfPrimeOrNot.java
@@ -0,0 +1,14 @@
+package com.example.javabasicrefresher;
+
+import java.util.Scanner;
+
+public class findIfPrimeOrNot {
+
+        public static void main(String[] args){
+            int temp, num;
+            boolean isPrime = true;
+            Scanner in = new Scanner(System.in);
+            num = in.nextInt();
+        }
+
+}
--- app/src/main/java/com/example/javabasicrefresher/swapTwoNumbersUsingThirdVariable.java
+++ app/src/main/java/com/example/javabasicrefresher/swapTwoNumbersUsingThirdVariable.java
@@ -1,11 +1,12 @@
 package com.example.javabasicrefresher;
+
 import java.util.Scanner;
 
 public class swapTwoNumbersUsingThirdVariable {
 
     public static void main(String[] args) {
 
-        int x,y, temp;
+        int x, y, temp;
         System.out.println("Enter x and y");
         Scanner in = new Scanner(System.in);
         x = in.nextInt();
