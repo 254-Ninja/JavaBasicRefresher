f034189e12dfeba7798ad8b12de073e188aa7cec
User <muthomidenis62@gmail.com>
2022-01-03 12:26:44 +0300

    Swapping two numbers using a third variable

--- app/src/main/java/com/example/javabasicrefresher/swapTwoNumbersUsingThirdVariable.java
+++ app/src/main/java/com/example/javabasicrefresher/swapTwoNumbersUsingThirdVariable.java
@@ -1,9 +1,11 @@
 package com.example.javabasicrefresher;
-import java.util.Scanner
+import java.util.Scanner;
 
 public class swapTwoNumbersUsingThirdVariable {
 
     public static void main(String[] args) {
 
+        int x,y, temp;
+        System.out.println("Enter x and y");
     }
 }
