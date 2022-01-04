500d65b257fcb2e7e7c4adcef36784f8f8d38104
User <muthomidenis62@gmail.com>
2022-01-04 15:13:45 +0300

    check if a string contains a vowel

--- app/src/main/java/com/example/javabasicrefresher/checkIfAVowelIsPresentInTheString.java
+++ app/src/main/java/com/example/javabasicrefresher/checkIfAVowelIsPresentInTheString.java
@@ -1,4 +1,11 @@
 package com.example.javabasicrefresher;
 
 public class checkIfAVowelIsPresentInTheString {
+    public static void main(String[]args){
+        System.out.println(checkIfAVowelIsPresentInTheString("Hello"));
+
+        //true
+        System.out.println(checkIfAVowelIsPresentInTheString("TV"));
+
+    }
 }
