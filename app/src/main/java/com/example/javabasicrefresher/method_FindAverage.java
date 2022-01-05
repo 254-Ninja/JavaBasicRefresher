728220b38bce587a689ed87ac4ce449d7fdb9118
User <muthomidenis62@gmail.com>
2022-01-05 20:02:25 +0300

    method to find average

--- app/src/main/java/com/example/javabasicrefresher/method_FindAverage.java
+++ app/src/main/java/com/example/javabasicrefresher/method_FindAverage.java
@@ -7,6 +7,10 @@ public class method_FindAverage {
         Scanner in = new Scanner(System.in);
         System.out.print("Input the first number: ");
         double x = in.nextDouble();
+        System.out.print("Input the first number: ");
+        double y = in.nextDouble();
+        System.out.print("Input the first number: ");
+        double z = in.nextDouble();
 
     }
 }
