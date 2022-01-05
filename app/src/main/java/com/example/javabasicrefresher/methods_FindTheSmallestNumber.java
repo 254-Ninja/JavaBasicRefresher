2283cad953cbb255f882df0f720c8b9b5c8bb599
User <muthomidenis62@gmail.com>
2022-01-05 19:34:01 +0300

    Find the smallest number among three numbers

--- app/src/main/java/com/example/javabasicrefresher/methods_FindTheSmallestNumber.java
+++ app/src/main/java/com/example/javabasicrefresher/methods_FindTheSmallestNumber.java
@@ -1,4 +1,11 @@
 package com.example.javabasicrefresher;
+import java.util.Scanner;
 
 public class methods_FindTheSmallestNumber {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the first number: ");
+        double x = in.nextDouble();
+    }
+
 }
