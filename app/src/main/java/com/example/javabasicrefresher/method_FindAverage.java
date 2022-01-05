f36c5c3f0f78b64344c0b41c6d6a973edb074252
User <muthomidenis62@gmail.com>
2022-01-05 20:00:40 +0300

    method to find average

---
+++ app/src/main/java/com/example/javabasicrefresher/method_FindAverage.java
@@ -0,0 +1,11 @@
+package com.example.javabasicrefresher;
+
+import java.util.Scanner;
+
+public class method_FindAverage {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the first number: ");
+
+    }
+}
