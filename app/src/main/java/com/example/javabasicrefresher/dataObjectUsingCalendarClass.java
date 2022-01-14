b7744fd2f546a8d0f9a274d168b0779e523ace2b
User <muthomidenis62@gmail.com>
2022-01-14 22:28:17 +0300

    Date object using the calendar class

--- app/src/main/java/com/example/javabasicrefresher/dataObjectUsingCalendarClass.java
+++ app/src/main/java/com/example/javabasicrefresher/dataObjectUsingCalendarClass.java
@@ -1,4 +1,8 @@
 package com.example.javabasicrefresher;
-
+//Date object using the calendar class
 public class dataObjectUsingCalendarClass {
+
+    public static void main(String[] args){
+
+    }
 }
