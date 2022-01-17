6916b8c61b7138b8d889ace2947b32985e4d3bdb
User <muthomidenis62@gmail.com>
2022-01-17 14:30:39 +0300

    Binary search sample

--- app/src/main/java/com/example/javabasicrefresher/binarySearch.java
+++ app/src/main/java/com/example/javabasicrefresher/binarySearch.java
@@ -1,4 +1,9 @@
 package com.example.javabasicrefresher;
-
+//write a Java program to find a specified element in a given array of elements using Binary Search
 public class binarySearch {
+    public static int binarySearch(int[] nums, int flag){
+        int hi_num = nums.length - 1;
+
+    }
+
 }
